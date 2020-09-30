6d169829bce4eef4c09eb86d2a34853b4dec373e
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-09-30 20:07:38 +0300

    Fixed mysql dependency starter error

--- spring-mysql/src/main/java/com/example/springmysql/SpringMysqlApplication.java
+++ spring-mysql/src/main/java/com/example/springmysql/SpringMysqlApplication.java
@@ -2,8 +2,9 @@ package com.example.springmysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class SpringMysqlApplication {
 
 	public static void main(String[] args) {
