7e0ada20e4bdee64922961605ff05dc89449a61c
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-09-30 22:33:51 +0300

    Added book controller update resource

--- spring-mysql/src/main/java/com/example/springmysql/BookController.java
+++ spring-mysql/src/main/java/com/example/springmysql/BookController.java
@@ -3,7 +3,9 @@ package com.example.springmysql;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +26,18 @@ public class BookController {
     @PostMapping("/books")
     public Book addBook(@RequestBody Book book) {
         return bookRepository.save(book);
+    }
+
+    @PutMapping("/books/{id}")
+    public Book updateBook(@RequestBody Book bookDetails, @PathVariable Long id) {
+        Book updatingBook = bookRepository.findById(id).orElseThrow(() -> BookNotFoundException(id));
+
+        updatingBook.setName(bookDetails.getName());
+        updatingBook.setAuthor(bookDetails.getAuthor());
+        updatingBook.setIsbn(bookDetails.getIsbn());
+
+        return bookRepository.save(updatingBook);
+
     }
 
 }
