41e3b8320dad58ca0b06974a8f5a7ce60bc25222
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-10-01 01:18:24 +0300

    Wired up the app with mysql db

--- spring-mysql/src/main/java/com/example/springmysql/SpringMysqlApplication.java
+++ spring-mysql/src/main/java/com/example/springmysql/SpringMysqlApplication.java
@@ -2,9 +2,9 @@ package com.example.springmysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+// import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class SpringMysqlApplication {
 
 	public static void main(String[] args) {
